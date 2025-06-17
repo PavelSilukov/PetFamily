d266baa3b81a15b892b9dbf686572bcb9b36657f
Pavel Silukov <434sp89@gmail.com>
2025-06-17 13:48:55 +0300

    Create Volunteer

--- petFamily.backend/src/petFamily.Domain/Volunteer/Volunteer.cs
+++ petFamily.backend/src/petFamily.Domain/Volunteer/Volunteer.cs
@@ -1,22 +1,57 @@
-﻿namespace petFamily.Domain.Volunteer;
+﻿using CSharpFunctionalExtensions;
+
+namespace petFamily.Domain.Volunteer;
 
 public class Volunteer
 {
-    // public Volunteer(string firstName, string lastName)
-    // {
-    //     FirstName = firstName;
-    //     LastName = lastName;
-    // }
-    // public Guid Id { get; set; }
-    // public string FirstName { get; private set; } 
-    // public string LastName { get; private set; }
-    // public string Email { get; set; }
-    // public int Experience { get; set; }
-    // public List<Pet> Pets { get; set; }
-    //
-    // public int getPetsFoundHome()
-    // {
-    //     return Pets.Count;
-    // }
-    // public Requisite requisite { get; set; }
+    private readonly List<Pet> _pets = [];
+    // ef
+    private Volunteer()
+    {
+
+    }
+
+    private Volunteer(string description)
+    {
+        Description = description;
+    }
+    public Guid Id { get; private set; }
+    public string FIO { get; private set; }
+    public string Email { get; private set;}
+    public string Description { get; private set; } = default!;
+    
+    public int ExperienceYears { get; private set;}
+    public IReadOnlyList<Pet> pets => _pets;
+    
+    public List<Pet> getPetsFoundHome()
+    {
+        return null;
+    }
+
+    public List<Pet> LookingForHome()
+    {
+        return null;
+    }
+    public List<Pet> NeedsHelp()
+    {
+        return null;
+    }
+    public Requisite requisite { get; set; }
+
+    public void AddPet(Pet pet)
+    {
+        _pets.Add(pet);
+    }
+    
+
+    public static Result<Volunteer> Create(string description)
+    {
+        if (string.IsNullOrWhiteSpace(description))
+        {
+            return Result.Failure<Volunteer>("Description cannot be empty");
+        }
+
+        var volunteer = new Volunteer(description);
+        return Result.Success(volunteer);
+    }
 }
