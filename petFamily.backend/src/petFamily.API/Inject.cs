dc87fea805c20ad6868ca535afadfe19565e8eee
Pavel Silukov <434sp89@gmail.com>
2025-08-26 15:43:05 +0300

    Inject in API

---
+++ petFamily.backend/src/petFamily.API/Inject.cs
@@ -0,0 +1,23 @@
+﻿using petFamily.API.Validation;
+using petFamily.Application;
+using petFamily.Infrastructure;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+
+namespace petFamily.API;
+
+public static class Inject
+{
+    public static IServiceCollection AddAPI (this IServiceCollection services)
+    {
+        services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        services.AddFluentValidationAutoValidation(configuration =>
+        {
+            configuration.OverrideDefaultResultFactoryWith<CustomResultFactory>();
+        });
+        return services;
+    }
+}
